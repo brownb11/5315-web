645df4122e347137c3d17da8de4fb78cc7b0b3ad
brownb11 <brownb11@seattleu.edu>
2017-04-17 22:46:50 -0700

    hw2 commit

---
+++ beer_data.js
@@ -0,0 +1,54 @@
+{
+  "Beer_Table": {
+    "BeerRow": [
+      {
+        "Name": "Bourbon Abominable",
+        "Style": "Winter Ale",
+        "Brewery": "Fremont Brewing",
+        "State": "WA",
+        "Country": "USA",
+        "Rating": "5"
+      },
+      {
+        "Name": "Kriek",
+        "Style": "Lambic",
+        "Brewery": "Cantilion",
+        "State": "Brussels",
+        "Country": "BEL",
+        "Rating": "4"
+      },
+      {
+        "Name": "Bud Light",
+        "Style": "Lager",
+        "Brewery": "Budweiser",
+        "State": "Johnaesberg",
+        "Country": "SA",
+        "Rating": "1"
+      },
+      {
+        "Name": "Asterisk",
+        "Style": "IPA",
+        "Brewery": "Melvin",
+        "State": "WY",
+        "Country": "USA",
+        "Rating": "5"
+      },
+      {
+        "Name": "Pliny the Elder",
+        "Style": "IPA",
+        "Brewery": "Russian River",
+        "State": "CA",
+        "Country": "USA",
+        "Rating": "5"
+      },
+      {
+        "Name": "Trappiste",
+        "Style": "Dubel",
+        "Brewery": "La Fin duMonde",
+        "State": "Calgary",
+        "Country": "CA",
+        "Rating": "4"
+      }
+    ]
+  }
+}
